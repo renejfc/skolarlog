2708e188a2260bd614413e082b7478c30c12a513
ReneJFC <hola@renejfc.dev>
2021-11-14 02:30:43 +0100

    component: toggle theme

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useTheme } from '~/logic/Shared/composables'
+
+const { themeShift, isDark } = useTheme()
+
+</script>
+
+<template>
+  <n-button
+    circle
+    size="large"
+    dashed
+    class="absolute right-8 top-6 z-50"
+    @click="themeShift()"
+  >
+    <span :class=" isDark ? 'i-carbon-moon' : 'i-carbon-sun'" />
+  </n-button>
+</template>
+
+<style scoped lang="css"></style>
