8afecfb5d2da9e94ec0678ef637b164fbf9d45c6
ReneJFC <hola@renejfc.dev>
2021-11-20 14:35:50 +0100

    chore(deps): remove innecesary deps & add scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
-import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
 import Unocss from 'unocss/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import { presetUno } from 'unocss'
@@ -40,20 +38,8 @@ export default defineConfig({
     Components({
       resolvers: [
         NaiveUiResolver(),
-
-        // https://github.com/antfu/vite-plugin-icons
-        IconsResolver({
-          enabledCollections: [
-            'carbon',
-          ],
-        }),
       ],
       dts: true,
-    }),
-
-    // https://github.com/antfu/vite-plugin-icons
-    Icons({
-      autoInstall: true,
     }),
 
     // https://github.com/antfu/unocss
